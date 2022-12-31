08e03f54b7efe9242803518e80e59cd4c025931f
José Gerstner <joseernestogerstnerjavid@gmail.com>
2022-12-30 21:39:55 -0300

    v0.3

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -5,7 +5,7 @@ function Encabezado() {
   return (
     <header className="bg-celeste">
         <h3 className='row py-md-4 py-2 anulo-gutter-x'></h3>
-        <Link to='/'><h3 className='row p-1 bg-white anulo-gutter-x'><img className='img-fluid' src="/images/scalonetamuseum.png" alt="header" /></h3></Link>
+        <Link to='/'><img className='img-fluid' src="/images/scalonetamuseum.png" alt="header" /></Link>
         <h3 className='row py-md-4 py-2 anulo-gutter-x'></h3>
     </header>
   )
